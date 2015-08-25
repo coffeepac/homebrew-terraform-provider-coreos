1357594f9b0aff08a551e83fa1caf5b07eae80b8
Marat G <maratoid@gmail.com>
2015-08-24 17:02:12 -0700

    url change

--- terraform-provider-coreos.rb
+++ terraform-provider-coreos.rb
@@ -1,7 +1,7 @@
 class TerraformProviderCoreos < Formula
   desc "Get latest coreos ami for region"
   homepage "https://github.com/bakins/terraform-provider-coreos"
-  url "https://s3-us-west-2.amazonaws.com/brewtaps/terraform-provider-coreos/terraform-provider-coreos.tar.gz"
+  url "https://github.com/Samsung-AG/homebrew-terraform-provider-execute/releases/download/v0.0.1/terraform-provider-coreos.tar.gz"
   version "0.0.1"
   sha256 "c8037e5698ffe692e8b1275209e74ed5070f157b23d9b857ac180c63595483b1"
 
