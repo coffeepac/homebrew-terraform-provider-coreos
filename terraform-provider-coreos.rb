c17e00462f180aa6936448ed74a3d0e3f693ad7d
Marat G <maratoid@gmail.com>
2015-08-26 16:13:20 -0700

    adding a built linux 64 binary to release required a rename

--- terraform-provider-coreos.rb
+++ terraform-provider-coreos.rb
@@ -1,7 +1,7 @@
 class TerraformProviderCoreos < Formula
   desc "Get latest coreos ami for region"
   homepage "https://github.com/bakins/terraform-provider-coreos"
-  url "https://github.com/Samsung-AG/homebrew-terraform-provider-coreos/releases/download/v0.0.1/terraform-provider-coreos.tar.gz"
+  url "https://github.com/Samsung-AG/homebrew-terraform-provider-coreos/releases/download/v0.0.1/terraform-provider-coreos_darwin_amd64.tar.gz"
   version "0.0.1"
   sha256 "c8037e5698ffe692e8b1275209e74ed5070f157b23d9b857ac180c63595483b1"
 
